0b65ec80a1bb9efce55f273a2911b24a852b9fb1
meusebio.depaz <meusebio.depaz@bbva.com>
2017-04-25 12:28:27 +0200

    Data typing -> pendiente de import

---
+++ 03_data_typing/data_typing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Mypy
+
+
+# def suma(a, b) -> válido pero confuso
+
+def suma(a: int, b:int) -> int:
+    return a + b
+
+# Ver fira code -> para que ponga flechitas :)
+
+def my_two_function(l: List[str]) -> Dict[str, int]:
+    d = {}
+    for i, x in enumerate(l):
+        d[str(i)] = x
+
+    return d
+
+
+r = my_two_function(1)
